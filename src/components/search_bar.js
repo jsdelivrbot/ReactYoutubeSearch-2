c73a1f696eca37d23ff2dba3fde3f844441f876d
Ruffeng <ruben.suet@gmail.com>
2017-02-18 11:36:07 +0100

    Setting states handling the input user event

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,9 +3,18 @@ import {Component} from "react";
 
 
 class SearchBar extends Component{
+  constructor(props){
+    super(props);
 
+    this.state = {term: ''};
+  }
   render(){
-    return (<input onChange={(event)=> console.log(event.target.value) } />);
+    return (
+      <div>
+        <p>Value of the input: {this.state.term}</p>
+        <input onChange={(event)=> this.setState({term: event.target.value}) } />
+      </div>
+     );
   };
 
 }
