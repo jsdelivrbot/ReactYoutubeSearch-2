30b7a0b81533b0f300c8972feab0885068813255
Ruffeng <ruben.suet@gmail.com>
2017-02-18 11:26:21 +0100

    Checking how to handler user events. Anything new so far...

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,9 +3,11 @@ import {Component} from "react";
 
 
 class SearchBar extends Component{
+
   render(){
-    return (<input />);
+    return (<input onChange={(event)=> console.log(event.target.value) } />);
   };
+
 }
 
 export default SearchBar;
