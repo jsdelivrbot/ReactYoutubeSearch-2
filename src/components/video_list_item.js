72fcb41662dd41fbd136702b36cf4f2f85c31c20
Ruffeng <ruben.suet@gmail.com>
2017-02-18 20:32:53 +0100

    Added some info in videListDetail component

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,10 +2,25 @@ import React,{Component} from "react";
 
 
 export default class VideoListItem extends Component{
-
+  constructor(props){
+    super(props)
+    this.video_info = this.props.video.snippet.description;
+    this.img_url = this.props.video.snippet.thumbnails.default.url;
+    this.video_title = this.props.video.snippet.title;
+  }
   render(){
     return(
-      <li>{this.props.video.snippet.description}</li>
+      <li className="list-group-item">
+        <div className="video-list media">
+          <div className="media-left">
+            <img className="media-object" src={this.img_url} />
+          </div>
+
+          <div className="media-body">
+            <div className="media-heading">{this.video_title}</div>
+          </div>
+        </div>
+      </li>
     );
   }
 }
