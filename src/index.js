03c26186258c7f057d0b551e601c54b6fd0339eb
Ruffeng <ruben.suet@gmail.com>
2017-02-18 12:00:42 +0100

    Starting to ask some inf to youtube API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React ,{Component}from 'react';
 import ReactDom from 'react-dom';
+// YOUTUBE Library
+import YTSearch from 'youtube-api-search';
+
+
 import SearchBar from './components/search_bar';
 
 const API_KEY =  "YOUR API";
+YTSearch({key: API_KEY, term: "surfboards"}, (data) => {
+  console.log(data);
+});
+
 
 class App extends Component{
   render(){
