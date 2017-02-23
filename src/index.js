4d64131e17be2ac51d3677175c5d6792c5944e44
Ruffeng <ruben.suet@gmail.com>
2017-02-23 10:51:38 +0100

    Created new branch

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
 import VideoDetail from './components/video_detail';
-import Secret from './secrets/secrets';
 
+// SECRETS TO INVOKE THE API YOUTUBE
+import Secret from './secrets/secrets';
 const API_KEY =  new Secret().api();
 
 
